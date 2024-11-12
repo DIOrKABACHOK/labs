2b8a69438f20aa8bcdbc24938c46d3174fa889bc
DIOr KABACHOK <98692544+DIOrKABACHOK@users.noreply.github.com>
2024-11-12 17:47:04 +0300

    Update lab5-2.py

--- lab5/lab5-2.py
+++ lab5/lab5-2.py
@@ -9,7 +9,14 @@ weights = np.array([0.299, 0.587, 0.114])
 
 gray_image = np.dot(image_array, weights)
 
+gray_image_uint8 = gray_image.astype(np.uint8)
+
+gray_image_pil = Image.fromarray(gray_image_uint8)
+
+gray_image_pil.save('gray_image.png')
+
 print("Исходное изображение:")
 print(image_array)
 print("\nПреобразованное изображение в оттенки серого:")
 print(gray_image)
+print("\nИзображение сохранено как 'gray_image.png'")
